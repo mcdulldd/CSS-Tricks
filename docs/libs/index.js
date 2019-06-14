faa29f15a9f5b27471d78894d028e2626d530cf8
mcdulldd <2953650860@qq.com>
2019-06-14 16:59:31 +0800

    delete gh-pages

---
+++ docs/libs/index.js
@@ -0,0 +1,19 @@
+window.$docsify = {
+  name: "CSS Tricks",
+  repo: "https://github.com/l-hammer/You-need-to-know-css",
+  auto2top: true,
+  loadSidebar: true,
+	loadNavbar: true,
+  mergeNavbar: true,
+  subMaxLevel: 2,
+  homepage: "README.md",
+  search: {
+    noData: {
+      "/": "找不到结果"
+    },
+    paths: "auto",
+    placeholder: {
+      "/": "搜索"
+    }
+  },
+};
